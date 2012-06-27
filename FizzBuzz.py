c59683e36926c44ae6e81f28de9d356cea26e8ae
Ashish-Yadav <ashish_yadav@persistent.co.in>
2012-06-27 16:22:06 +0530

    Update master

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,14 @@
 """
 Q1. Why is the report method untestable ? [2 pts]
-
+Ans:- The report method has external depndencies.
+  It depends on the "open function" and the Txt file.
 
 
 
 Q2. How will you change the api of the report method to make it more testable ? [2 pts]
-
-
+Ans:- The API of the report method can be changed by the the number of arguments it accepts as
+    "report(self,numbers,filehandler)"
+where "filehandler" will be used for creating an impression of reading values from file.
 
 """
 class FizzBuzz(object):
